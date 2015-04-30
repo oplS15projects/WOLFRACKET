2d2360eac78352dc9fc4cd8fde0bf9e5b725b0ce
Jose Flores <jose.flores.152@gmail.com>
2015-04-29 17:20:17 -0700

    M2

--- wolfracket/lib/api.rkt
+++ wolfracket/lib/api.rkt
@@ -4,8 +4,12 @@
 ;   This file holds the api.
 
 ;   REQUIRES
-(require web-server/servlet
+(require racket/include
+         web-server/servlet
          web-server/servlet-env)
+
+(include "param-handler.rkt")
+(include "math.rkt")
 
 ;   @name api
 ;
@@ -23,20 +27,65 @@
 ;   @return     json    the calculated output or error message
 (define (api)
 
+
+
     ;   RESPONSES
-    ;   The api documentation page
+    ;   The API Documentation page
     (define (api-home req)
             (response/xexpr
                 `(html  (head (title "API"))
                         (body (p "Racket Mathematics and Graphing API Project Documentation Page")))))
 
-    ;   The addition api method
-    (define (api-add req)
-            (response/xexpr
-                `(html  (head (title "ADD"))
-                        (body (p "Addition method")))))
+    ;   API Function Execution
+    (define (api-func method param min-param req)
+        (response/xexpr
+                #:mime-type #"application/json"
+                (string-join (list "["
+                                    (((param-handler) "list->string" )
+                                        (if (param-count? min-param (((param-handler) "string->list") req
+                                                                                            param))
+                                            (((my_math) method) (((param-handler) "string->list") req
+                                                                                            param))
+                                            (list "\"error\"")))
+                                    "]")
+                             "")))
 
-    ;   The method not found page
+    (define (param-count? min-param lst)
+        (>= (length lst) min-param))
+
+    ;   The API Methods
+    (define (api-add req)
+        (api-func "add" "j" 2 req))
+
+    (define (api-subtract req)
+        (api-func "subtract" "j" 2 req))
+
+    (define (api-exponent req)
+        (api-func "exponent" "j" 2 req))
+
+    (define (api-divide req)
+        (api-func "divide" "j" 2 req))
+
+    (define (api-prime req)
+        (api-func "prime" "j" 1 req))
+
+    (define (api-logarithm req)
+        (api-func "logarithm" "j" 1 req))
+
+    (define (api-factorial req)
+        (api-func "factorial" "j" 1 req))
+
+    (define (api-sin req)
+        (api-func "sin" "j" 1 req))
+
+    (define (api-cos req)
+        (api-func "cos" "j" 1 req))
+
+    (define (api-sqrt req)
+        (api-func "sqrt" "j" 1 req))
+
+
+    ;   The Method not found page
     (define (api-404 req)
             (response/xexpr
                 `(html  (head (title "404"))
@@ -44,11 +93,20 @@
 
     ;   CONTROLLER
     ;   Delegates which method to call
-    (define (controller m)
-        (begin  (display m)
-        (cond ((equal? m "home") api-home)
-              ((equal? m "add") api-add)
-              (else api-404))))
+    (define (controller action p)
+        (begin  ;(set! msg m)
+                (cond ((equal? action "home") api-home)
+                      ((equal? action "add") api-add)
+                      ((equal? action "subtract") api-subtract)
+                      ((equal? action "exponent") api-exponent)
+                      ((equal? action "divide") api-divide)
+                      ((equal? action "prime") api-prime)
+                      ((equal? action "logarithm") api-logarithm)
+                      ((equal? action "factorial") api-factorial)
+                      ((equal? action "sin") api-sin)
+                      ((equal? action "cos") api-cos)
+                      ((equal? action "sqrt") api-sqrt)
+                      (else api-404))))
 
     controller)
 
--- wolfracket/lib/server-handler.rkt
+++ wolfracket/lib/server-handler.rkt
@@ -8,8 +8,12 @@
          web-server/servlet
          web-server/servlet-env
          web-server/dispatch
-         web-server/dispatchers/dispatch-servlets)
+         web-server/dispatchers/dispatch-servlets
+         net/url-structs
+         web-server/formlets/input)
 
+(require net/url)
+(require web-server/http/request-structs)
 ;   Including the api, this is needed because the server has an api interface
 (include "api.rkt")
 
@@ -31,31 +35,88 @@
     ;   The home page
     (define (home req)
         (response/xexpr
-            `(html  (head (title "Home"))
-                    (body (p "Racket Mathematics and Graphing API Project Page")))))
+            `(html  (head   (meta ((charset "utf-8")))
+                            (title "Home")
+                            (link ([rel "stylesheet"]
+                                   [type "text/css"]
+                                   [href "css/main.css"]))
+                            (script ((src "http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js")))
+                            (script ([type "application/javascript"]
+                                   [src "js/main.js"]))
+
+                    (body   (div ((class "header"))
+                                (div ((class "title"))
+                                     (img ([class "logo"]
+                                          [src "http://www.eecs.northwestern.edu/~robby/logos/plt-logo-red-diffuse.png"]))
+                                     "Racket Mathematics and Graphing API Project Page")
+
+                                (div ((class "menu"))
+                                     (ul (li (a ((href "/home")) "home"))
+                                         (li (a ((href "/api")) "api documentation")))))
+
+                                (div ((class "content"))
+                                     (h2 "About the Racket Mathematics and Graphing API")
+                                     (p "The API is run by a Racket web server and uses racket to produce results. Below is a test form that can be used to see the output of the Racket API.")
+                                     (h2 "Test the API")
+                                     (form (select ((id "method"))
+                                                   (option ((value "add")) "add")
+                                                   (option ((value "subtract")) "subtract")
+                                                   (option ((value "exponent")) "exponent")
+                                                   (option ((value "divide")) "divide")
+                                                   (option ((value "prime")) "prime")
+                                                   (option ((value "logarithm")) "logarithm")
+                                                   (option ((value "factorial")) "factorial")
+                                                   (option ((value "sin")) "sin")
+                                                   (option ((value "cos")) "cos")
+                                                   (option ((value "sqrt")) "sqrt"))
+
+                                            (input ((id "num1")))
+                                            (input ((id "num2")))
+                                            (input ((id "num3")))
+                                            (button ([id "button"]
+                                                     [type "button"]
+                                                     [onClick "mysubmit();"]) "Submit"))
+                                    (h2 "Results")
+                                    (p ((id "result")) "Submit for a value")))))))
+
     ;   404 page
     (define (my-404 req)
             (response/xexpr
                 `(html  (head (title "404"))
-                        (body (p "Resource not found.")))))
+                        (body (p "Resource not found, api not accessed.")))))
 
     ;   API INTERFACE
-    (define (api-interface req [method "home"])
-        (begin  (display method)
-                (((api) method) req)))
+    (define (api-interface req [method "home"] [param "none"])
+        ;(begin (display req)
+                (((api) method param) req));)
+
 
     ;   URL DISPATCHING
     ;   Recieve and process request
     (define (start request)
         (dispatch request))
 
+    (define (serve-css req [arg ""])
+        (response 200 #"OK" 0 #"text/css" empty (lambda (op)
+            (with-input-from-file arg (lambda () (copy-port
+                (current-input-port) op))))))
+
+    (define (serve-js req [arg ""])
+        (response 200 #"OK" 0 #"application/javascript" empty (lambda (op)
+            (with-input-from-file arg (lambda () (copy-port
+                (current-input-port) op))))))
+
     ;   Determine url
     (define-values (dispatch url)
+        (begin (display url)
         (dispatch-rules
             [("home") home]
+            [("css" (string-arg)) serve-css]
+            [("js" (string-arg)) serve-js]
             [("api") api-interface]
             [("api" (string-arg)) api-interface]
-            [else my-404]))
+            [("api" (string-arg) (string-arg)) api-interface]
+            [else my-404])))
 
     ;   SERVER
     ;   start server
@@ -63,7 +124,8 @@
             (serve/servlet start
                 #:port 8080
                 #:listen-ip #f          ;   listen for external requests
-                #:servlet-path ""
+                #:servlet-path "/"
+                #:extra-files-paths (list "./htdocs")
                 #:servlet-regexp #rx""))
 
     ;   CONTROLLER
